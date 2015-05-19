060d1e3b9a29d2f3b97185809da5d2293d320d5b
devanastiko <devan.astiko@gmail.com>
2015-05-19 14:16:44 +0700

    doasdkasdasdnlasdnkl

---
+++ application/models/loginAkun.php
@@ -0,0 +1,22 @@
+<?php
+
+class loginAkun extends CI_Model {
+
+    public function __construct() {
+        // Call the CI_Model constructor
+        parent::__construct();
+    }
+
+    public function getTrue($username, $password) {
+        $query = $this->db->query("SELECT usernameAkun, passwordAkun "
+                . "FROM akun WHERE usernameAkun= '" . $username . "' AND passwordAkun= '" . $password . "'");
+
+
+        if ($query->num_rows() > 0) {
+                return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+}
---
+++ application/views/admin.php
@@ -0,0 +1,46 @@
+<div class="content"><div class="ic"></div>
+    <div class="container_12">
+        <div class="grid_12">
+            <h3><span>Admin Page</span></h3></div>
+        <div class="grid_5">
+            <div class="map">
+                <figure class="">
+                    <iframe src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Brooklyn,+New+York,+NY,+United+States&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=Brooklyn,+Kings,+New+York&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed">
+                    </iframe>
+                </figure>
+                <p>Our free themes go without support services. Support services and special offers come with premium <span class="col1"><a href="http://www.templatemonster.com/category.php" rel="nofollow">website templates.</a></span></p>
+                <p>If you need help in customization of this theme, hire guys from <span class="col1"><a href="http://templatetuning.com" rel="dofollow">Template Tuning</a></span> to do it for you.</p>Leave us comments with any ideas about free website templates.
+            </div>              
+        </div>
+        <div class="grid_6 prefix_1">
+            <form id="form" action="Respond">               
+                <fieldset>
+                    <label class="name">
+                        <input type="text" name="name" placeholder="Name:" class="field">
+                        <br class="error">
+                        <span class="error error-empty">*This is not a valid name.</span><span class="empty error-empty">*This field is required.</span> </label>
+                    <label class="phone">
+                        <input type="tel" placeholder="Telephone:" class="field">
+                        <br class="clear">
+                        <span class="error error-empty">*This is not a valid phone number.</span><span class="empty error-empty">*This field is required.</span> </label>
+                    <label class="email">
+                        <input type="text" placeholder="E-mail:" class="field">
+                        <br class="clear">
+                        <span class="error error-empty">*This is not a valid email address.</span><span class="empty error-empty">*This field is required.</span> </label>
+                    <label class="message">
+                        <textarea>Message:</textarea>
+                        <br class="clear">
+                        <span class="error">*The message is too short.</span> <span class="empty">*This field is required.</span> </label>
+                    <div class="clear"></div>
+                    <div class="btns">
+                        <input type="reset" name="reset" value="Reset" class="btn-grey"><br><br>
+                        <input type="submit" name="submit" value="Submit" class="btn-red">
+                        <div class="clear"></div>
+                    </div></fieldset></form>
+            Marketing Department: <br>
+            E-mail: <span class="col1"><a href="#">marketing@templatemonster.com</a></span> <br>
+            Phone: 1-518-312-4162
+
+        </div>
+    </div>
+</div>
