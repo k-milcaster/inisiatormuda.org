d02865f12c5525e0d9992c607033d0ff68e690ba
K-MiL Caster <k.cas7er@gmail.com>
2015-05-25 23:05:49 +0700

    edit slider

--- application/views/home.php
+++ application/views/home.php
@@ -1,10 +1,25 @@
 <div class="slider_wrapper">
     <div id="camera_wrap" class="">
-        <div data-src="public/images/slide.jpg">
+        <div data-src="<?php echo base_url(); ?>public/images/slide.jpg">
+            <div class="caption">
+                <div class="slide-text-info">
+                    <h1>RECENTLY POST NEWS</h1>    
+                </div>
+            </div>
         </div>
-        <div data-src="public/images/slide1.jpg">
+        <div data-src="<?php echo base_url(); ?>public/images/slide1.jpg">
+            <div class="caption">
+                <div class="slide-text-info">
+                    <h1>RECENTLY POST</h1>    
+                </div>
+            </div>
         </div>
-        <div data-src="public/images/slide2.jpg">
+        <div data-src="<?php echo base_url(); ?>public/images/slide2.jpg">
+            <div class="caption">
+                <div class="slide-text-info">
+                    <h1>BEGINIKAH?</h1>    
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -18,20 +33,20 @@
         </div>
         <div class="grid_4">
             <div class="icon">
-                <img src="public/images/icon1.png" alt="">
+                <img src="<?php echo base_url(); ?>public/images/icon1.png" alt="">
                 <div class="title">ILUSTRASI</div>Paragraf bisa diisi apapun terserah
             </div>
         </div>
         <div class="grid_4">
             <div class="icon">
-                <img src="public/images/icon2.png" alt="">
+                <img src="<?php echo base_url(); ?>public/images/icon2.png" alt="">
                 <div class="title">ILUSTRASI</div>Paragraf bisa diisi apapun terserah <span class="col1"><a href="#" rel="dofollow">Ini link</a></span> inisatorMuda.
                 dikembangkan oleh <span class="col1"><a href="#" rel="nofollow">K-Software Development</a></span> masih belum ada link tujuan. 
             </div>
         </div>
         <div class="grid_4">
             <div class="icon">
-                <img src="public/images/icon3.png" alt="">
+                <img src="<?php echo base_url(); ?>public/images/icon3.png" alt="">
                 <div class="title">ILUSTRASI</div>Paragraf bisa diisi apapun terserah            
             </div>
         </div>
@@ -40,20 +55,20 @@
         </div>
         <div class="clear"></div>
         <div class="works">
-            <div class="grid_4"><a href="#"><img src="public/images/page1_img1.jpg" alt=""></a></div>
-            <div class="grid_4"><a href="#"><img src="public/images/page1_img1.jpg" alt=""></a></div>
-            <div class="grid_4"><a href="#"><img src="public/images/page1_img1.jpg" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
             <div class="clear"></div>
-            <div class="grid_4"><a href="#"><img src="public/images/page1_img1.jpg" alt=""></a></div>
-            <div class="grid_4"><a href="#"><img src="public/images/page1_img1.jpg" alt=""></a></div>
-            <div class="grid_4"><a href="#"><img src="public/images/page1_img1.jpg" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
         </div>
         <div class="clear"></div>
         <div class="grid_12">
             <h3><span>JUDUL LAGI</span></h3></div>
         <div class="grid_6">
             <blockquote>
-                <img src="public/images/page1_img7.jpg" alt="" class="img_inner fleft">
+                <img src="<?php echo base_url(); ?>public/images/page1_img7.jpg" alt="" class="img_inner fleft">
                 <div class="extra_wrapper">
                     <p>Kalimat sedikit aja</p>
                     <span class="col2 upp">Bule 1  </span>
@@ -62,7 +77,7 @@
         </div>
         <div class="grid_6">
             <blockquote>
-                <img src="public/images/page1_img8.jpg" alt="" class="img_inner fleft">
+                <img src="<?php echo base_url(); ?>public/images/page1_img8.jpg" alt="" class="img_inner fleft">
                 <div class="extra_wrapper">
                     <p>Kalimat sedikit aja</p>
                     <span class="col2 upp">Bule 2  </span>
