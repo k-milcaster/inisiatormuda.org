d07f46e26baaab516b28137277aa59d2e1d2a12b
devanastiko <devan.astiko@gmail.com>
2015-06-18 11:24:14 +0700

    tolong di up juga mil abis u edit ya ya ya hahhaha lg spk nich
    Editen lagi tentang
    1. our program di home itu fotonya g sama buaten sama mil pake css atau cropfoto aja
    2. Board Of advisor tulisannya d ganti nama advisor namanya sama di nama fotonya fotonya juga resize en lg mil ndak punya potochop soalnya

--- application/views/home.php
+++ application/views/home.php
@@ -1,23 +1,17 @@
 <div class="slider_wrapper">
     <div id="camera_wrap" class="">
-        <div data-src="<?php echo base_url(); ?>public/images/slide.jpg">
-            <div class="caption">
-                <div class="slide-text-info">
-                    <h1>RECENTLY POST NEWS</h1>    
-                </div>
-            </div>
-        </div>
-        <div data-src="<?php echo base_url(); ?>public/images/slide1.jpg">
+       
+        <div data-src="<?php echo base_url(); ?>public/images/slider/slide1.jpg">
             <div class="caption">
                 <div class="slide-text-info">
                     <h1>RECENTLY POST</h1>    
                 </div>
             </div>
         </div>
-        <div data-src="<?php echo base_url(); ?>public/images/slide2.jpg">
+        <div data-src="<?php echo base_url(); ?>public/images/slider/slide2.jpg">
             <div class="caption">
                 <div class="slide-text-info">
-                    <h1>BEGINIKAH?</h1>    
+                    <h1>Judicial Review UU Perkawinan</h1>    
                 </div>
             </div>
         </div>
@@ -28,47 +22,45 @@
 <div class="content"><div class="ic"></div>
     <div class="container_12">
         <div class="grid_12">
-            <h2>INI HEADLINE<span>SUB HEADLINE <span class="col1">WARNA LAEN</span> TERSERAH MAU DIISI APA </span></h2>
-            <h3><span>SEBUAH JUDUL</span></h3>
+            <h2>CORE PROGRAMS<span>Beberapa program yang kami lakukan diantaranya </span></h2>
+            <h3><span>Our Activity</span></h3>
         </div>
         <div class="grid_4">
             <div class="icon">
-                <img src="<?php echo base_url(); ?>public/images/icon1.png" alt="">
-                <div class="title">ILUSTRASI</div>Paragraf bisa diisi apapun terserah
+                <img src="<?php echo base_url(); ?>public/images/act/Edukasi.jpg" alt="">
+                <div class="title">EDUKASI</div>
             </div>
         </div>
         <div class="grid_4">
             <div class="icon">
-                <img src="<?php echo base_url(); ?>public/images/icon2.png" alt="">
-                <div class="title">ILUSTRASI</div>Paragraf bisa diisi apapun terserah <span class="col1"><a href="#" rel="dofollow">Ini link</a></span> inisatorMuda.
-                dikembangkan oleh <span class="col1"><a href="#" rel="nofollow">K-Software Development</a></span> masih belum ada link tujuan. 
-            </div>
+                <img src="<?php echo base_url(); ?>public/images/act/Legal.jpg" alt="">
+                <div class="title">LEGAL</div></div>
         </div>
         <div class="grid_4">
             <div class="icon">
-                <img src="<?php echo base_url(); ?>public/images/icon3.png" alt="">
-                <div class="title">ILUSTRASI</div>Paragraf bisa diisi apapun terserah            
+                <img src="<?php echo base_url(); ?>public/images/act/Riset.jpg" alt="">
+                <div class="title">RISET</div>
             </div>
         </div>
         <div class="grid_12">
-            <h3><span>Gambar-gambar ngawur</span></h3>
+            <h3><span>Media Coverage</span></h3>
         </div>
         <div class="clear"></div>
         <div class="works">
-            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
-            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
-            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/liputan/Detik.jpg" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/liputan/Kompas.png" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images//liputan/Liputan6.jpg" alt=""></a></div>
             <div class="clear"></div>
-            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
-            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
-            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/page1_img1.jpg" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/liputan/Okezone.png" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/liputan/Republika.jpg" alt=""></a></div>
+            <div class="grid_4"><a href="#"><img src="<?php echo base_url(); ?>public/images/liputan/Tempo.png" alt=""></a></div>
         </div>
         <div class="clear"></div>
         <div class="grid_12">
-            <h3><span>JUDUL LAGI</span></h3></div>
+            <h3><span>Board Of Advisor</span></h3></div>
         <div class="grid_6">
             <blockquote>
-                <img src="<?php echo base_url(); ?>public/images/page1_img7.jpg" alt="" class="img_inner fleft">
+                <img src="<?php echo base_url(); ?>public/images/advisor/RanggaWidigda.jpg" alt="" class="img_inner fleft">
                 <div class="extra_wrapper">
                     <p>Kalimat sedikit aja</p>
                     <span class="col2 upp">Bule 1  </span>
@@ -77,10 +69,11 @@
         </div>
         <div class="grid_6">
             <blockquote>
-                <img src="<?php echo base_url(); ?>public/images/page1_img8.jpg" alt="" class="img_inner fleft">
+                <img src="<?php echo base_url(); ?>public/images/advisor/DamianAgataYuvens.jpg" alt="" class="img_inner fleft">
                 <div class="extra_wrapper">
                     <p>Kalimat sedikit aja</p>
                     <span class="col2 upp">Bule 2  </span>
+                    
                 </div>
             </blockquote>
         </div>
