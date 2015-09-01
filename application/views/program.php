2aef5ed0abd7274f5493fc0419219eed093c60cc
K-MiL Caster <k.cas7er@gmail.com>
2015-09-01 18:44:24 +0700

    Bikin Pagination

--- application/views/program.php
+++ application/views/program.php
@@ -1,8 +1,25 @@
 <div class="content"><div class="ic"></div>
     <div class="container_12">
         <div class="grid_12">
-            <h3><span>Our Portfolio</span></h3>
+            
         </div>
+        <br>
+        <div class="clear"></div>
+        <section>
+            <nav role="navigation">
+                <ul class="cd-pagination no-space">
+                    <li class="button"><a href="#0">Prev</a></li>
+                    <li><a href="#0">1</a></li>
+                    <li><a href="#0">2</a></li>
+                    <li><a class="current" href="#0">3</a></li>
+                    <li><a href="#0">4</a></li>
+                    <li><span>...</span></li>
+                    <li><a href="#0">20</a></li>
+                    <li class="button"><a href="#0">Next</a></li>
+                </ul>
+            </nav> <!-- cd-pagination-wrapper -->
+        </section>
+        <br>
         <div class="clear"></div>
         <div class="port">
             <div class="grid_4">
