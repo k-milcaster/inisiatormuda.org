d44ca2995b2c9f1533a1cc2988ae321a1d7ffc43
K-MiL Caster <k.cas7er@gmail.com>
2015-09-10 14:23:03 +0700

    revisi tampilan

--- application/views/direktoriarticle.php
+++ application/views/direktoriarticle.php
@@ -1,22 +1,25 @@
 <body  class="">
-        <div class="content"><div class="ic"></div>
-    <div class="container_12">
-        
-        
-        <a href="<?php base_url()?>articles " class="btn">BUAT ARTIKEL</a>
+    <div class="content"><div class="ic"></div>
+        <div class="container_12">
 
-        
-        <h2>ARTICLE ORGANIZER</h2>
-        <br>
-        <h5>Recomended Di batasi untuk 4 artikel saja</h5>
-        <br>
-        <div class="clear"></div>
-                    <?php
-                    if (isset($errs)) {
-                        echo '<h5>' . $err . '</h5>';
-                    }
-                    ?>
-        <table>
+
+
+
+
+            <h2>ARTICLE ORGANIZER</h2>
+            <br>
+            <div class="grid_2">
+                <a href="<?php base_url() ?>articles "><div class="btn-red">Write New...</div></a>
+            </div>                
+            <h5><p align="right">Recomended Di batasi untuk 4 artikel saja</p></h5>
+            <br>
+            <div class="clear"></div>
+            <?php
+            if (isset($errs)) {
+                echo '<h5>' . $err . '</h5>';
+            }
+            ?>
+            <table>
                 <thead>
                     <tr><th>NO</th>
                         <th>DATE</th>
@@ -24,48 +27,49 @@
                         <th>ARTICLE</th>
                         <th>UPDATE ART</th>
                         <th>DELETE ART</th>
-                        <th>RECOMMEND</th><th>PUBLISH</th>
+                        <th>PUBLISH</th>
+                        <th>RECOMMEND</th>
                     </tr>
                 </thead>
                 <tbody>
-                    
-                    
-            <?php
-                                    $table = $this->session->flashdata('tableart');
 
-                                    echo $table;
-                                    ?>
-                    
-                    
-                    
-                    
-                    
-                    
-                    
+
+                    <?php
+                    $table = $this->session->flashdata('tableart');
+
+                    echo $table;
+                    ?>
+
+
+
+
+
+
+
                 </tbody>
-        
-        </table>
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-      
 
+            </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
     </div>
-</div>
