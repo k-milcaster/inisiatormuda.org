cd192f7203671d9c9d812c4970a942e45448d4ab
K-MiL Caster <k.cas7er@gmail.com>
2015-09-13 02:18:50 +0700

    Penutupan

--- application/controllers/Partner.php
+++ application/controllers/Partner.php
@@ -9,6 +9,7 @@ class Partner extends CI_Controller {
         $status = $this->systemmodel->getStatus('wholeweb');
         $statussingle = $this->systemmodel->getStatus('programs');
         $id = $this->uri->segment(3);
+        echo $id;
         foreach ($status->result() as $row) {
             foreach ($statussingle->result() as $row2) {
                 if ($row->status === '1' || $row2->status === '1') {
@@ -44,44 +45,6 @@ class Partner extends CI_Controller {
         }
     }
 
-    public function page() {
-        $this->load->model('systemmodel');
-        $status = $this->systemmodel->getStatus('wholeweb');
-        $statussingle = $this->systemmodel->getStatus('programs');
-        $page = $this->uri->segment(3);
-        if ($page == 1 || $page == 0) {
-            redirect(base_url() . "program");
-        }
-        foreach ($status->result() as $row) {
-            foreach ($statussingle->result() as $row2) {
-                if ($row->status === '1' || $row2->status === '1' || !isset($page)) {
-                    $this->load->view('maintenance');
-                } else {
-                    $this->load->model('programsmodel');
-                    $programs = $this->programsmodel->getProgramsList();
-                    $pagecount = (int) ($programs->num_rows() / 6);
-                    if ($pagecount % 6 != 0) {
-                        $pagecount++;
-                    }
-                    if ($page > $pagecount) {
-                        redirect(base_url() . "program/page/" . $pagecount);
-                    }
-                    $header = array(
-                        'title' => 'Programs',
-                    );
-                    $content = array(
-                        'pagecount' => $pagecount,
-                        'page' => $page,
-                        'programs' => $programs,
-                    );
-                    $this->load->view('header', $header);
-                    $this->load->view('program', $content);
-                    $this->load->view('footer');
-                }
-            }
-        }
-    }
-
     public function id() {
         $this->load->model('systemmodel');
         $status = $this->systemmodel->getStatus('wholeweb');
@@ -93,18 +56,18 @@ class Partner extends CI_Controller {
                     $this->load->view('maintenance');
                 } else {
                     if (!isset($id)) {
-                        redirect(base_url() . 'program');
+                        redirect(base_url() . 'partner');
                     } else {
-                        $this->load->model('programsmodel');
-                        $prog = $this->programsmodel->getProgram($id);
+                        $this->load->model('partnermodel');
+                        $partners = $this->partnermodel->getPartner($id);
                         $header = array(
-                            'title' => 'Programs',
+                            'title' => 'Partners',
                         );
                         $content = array(
-                            'prog' => $prog,
+                            'partners' => $partners,
                         );
                         $this->load->view('header', $header);
-                        $this->load->view('programsingle', $content);
+                        $this->load->view('partnersingle', $content);
                         $this->load->view('footer');
                     }
                 }
--- application/views/partnersingle.php
+++ application/views/partnersingle.php
@@ -1,12 +1,12 @@
 <div class="content"><div class="ic"></div>
     <div class="container_12">
-        <div class="grid_12">                
+        <div class="grid_12">    
             <?php
             foreach ($partners->result() as $row) {
                 echo "<h3><span>" . $row->name . "</span></h3></div>";
                 echo '<p align="center"><img id="blah" src="' . base_url() . 'public/images/partners/' . $row->img . '" alt="your image"/></p>';
                 echo "<p><b>Description : </b><br>"
-                . "" . $row->description . "</p>";
+                . "" . $row->desc . "</p>";
             }
             ?>
             <div class="clear"></div>                                         
